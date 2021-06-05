f74496be4fd44347a28158e70829fe2c91dd60a0
rijuthm@gmail.com <rijuthm@gmail.com>
2021-06-05 22:13:47 +0530

    relocated index.js and fixing import error
    module not found import error, cuz of CDN not loading in

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,17 @@
 "use strict";
 import CryptoJS from "crypto-js";
 
-function EncryptText(text, password) {
+// Function to encrypt text
+function encryptText(text, password)
+{
+  password = "hi";
   return CryptoJS.AES.encrypt(text, password).toString();
 }
 
-function decryptText(ciphertext, password) {
+// Function to decrypt text
+function decryptText(ciphertext, password)
+{
+  password = "hi";
   const bytes = CryptoJS.AES.decrypt(ciphertext, password);
   const originalText = bytes.toString(CryptoJS.enc.Utf8);
   return originalText;
