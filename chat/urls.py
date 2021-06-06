343621d7fd391df708fb94b74eeebbf371ec3101
rijuthm@gmail.com <rijuthm@gmail.com>
2021-06-06 19:14:09 +0530

    added room auth systm

--- chat/urls.py
+++ chat/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'chat'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('<str:room_name>/', views.room, name='room'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,21 +1,87 @@
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django import urls
+import hashlib
+import pymongo
+import string
+import random
+import datetime
 
+db_client = pymongo.MongoClient(
+    'mongodb+srv://markis:cmritproject123@cluster0.313vp.mongodb.net/priv?authSource=admin&replicaSet=atlas-fkelf3-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true')
+# Selecting database
+db = db_client['privmsg']
+col = db["rooms"]
 
 # Function to handle the form to create room
 def index(request):
+    # Handling form request
     if request.method == 'POST':
-        request.session['password_lol'] = request.POST.get('password')
-        return redirect(reverse('chat:room', kwargs={"room_name": request.POST.get('roomname')}))
+        # Password was not set
+        if not request.method.POST.get('password'):
+            return render(request, 'chat/index.html', {'error': True})
+        # Username was not set
+        elif not request.method.POST.get('username'):
+            return render(request, 'chat/index.html', {'error': True})
+        # Everything set, adding to db then sending to a burner room!
+        else:
+            # Generating a random room name and inserting to db
+            room_name = gen_room()
+            # Hashing the password
+            room_pass = hashlib.sha256(request.POST.get('password').encode()).hexdigest())
+            data = {'room_name':room_name, 'password':room_pass 'created_time': datetime.datetime.utcnow()}
+            col.insert(data)
+            # Setting a session with room_name so HOPEFULLY no one fakes it
+            request.session['room_name'] = room_name
+            # Temp session variable to grant access without auth
+            request.session['admin_bypass'] = True
+            return redirect(reverse('chat:room', kwargs={"room_name": request.POST.get('room_name')}))
+    # Handling GET request
     else:
-        return render(request, 'chat/index.html')
+        return render(request, 'chat/index.html', {'error': False})
 
-
+# Function to render room page, rest of it is handled by websocket connection!
 def room(request, room_name):
-    if 'password_lol' not in request.session:
-        return HttpResponse("Madarchod")
+    # Handling GET request:
+    if request.method == 'GET':
+        # Granting page access if user created the room and was redirected
+        if request.session['room_name'] and request.session['room_name'] == room_name:
+            # Deleting variable so he will have to relogin next time
+            del request.session['room_name']
+            return render(request, 'chat/room.html', {'room_name': room_name})
+        else:
+            return HttpResponse('Room login page!')
+    # User submitted form to login, authenticating him
+    elif request.method == 'POST':
+        # Checking if it's not a spam post request 
+        if not request.POST.get('password'):
+            return HttpResponse('No password error!')
+        # Checking room exists in db
+        room = col.find_one({'room_name':room_name}, {'_id':0, 'created_at':0})
+        if room is None:
+            return HttpResponse('Room does not exist!')
+        else:
+            # Checking if password is same
+            room_pass = hashlib.sha256(request.POST.get('password').encode()).hexdigest())
+            if room_pass == room['password']:
+                return render(request, 'chat/room.html', {'room_name': room_name})
+            else:
+                return HttpResponse('Room password wrong!')
+            
+
+# Function to generate a random room name
+def gen_room(length=random.randint(5, 7)):
+    # List of strings that can be used for a password
+    text = f"{string.ascii_letters}{string.digits}"
+    text = list(text)
+    # Shuffling the text
+    random.shuffle(text)
+    # Returning a randomly chosen password
+    room_name =  ''.join(random.choices(text, k=length))
+    # Checking if room name exists
+    db_unique = db['data'].find_one({'room_name':room_name})
+    # Exists, so making a recursive call
+    if db_unique:
+        gen_room()
     else:
-        return render(request, 'chat/room.html', {
-            'room_name': room_name
-        })
+        return room_name
