233212314bc9d9584b3aa4349cf284561501783d
rijuthm@gmail.com <rijuthm@gmail.com>
2021-06-11 03:06:10 +0530

    replaced chat with room

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
-    path('chat/', include('chat.urls')),
+    path('room/', include('chat.urls')),
     path('admin/', admin.site.urls),
 ]
