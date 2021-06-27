1db56b2e2c0127dc83371c87033abb22a05346a8
rijuthm@gmail.com <rijuthm@gmail.com>
2021-06-27 18:45:35 +0530

    integration completed!!!

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('tos', views.tos, name='tos'),
     path('room/', include('chat.urls')),
     path('msg/', include('msg.urls'))
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render, redirect, reverse
 # Landing page
 def index(request):
     return render(request, 'home/home.html')
+
+# About page
+def about(request):
+    return render(request, 'home/about.html')\
+
+# TOS page
+def tos(request):
+    return render(request, 'home/tos.html')
